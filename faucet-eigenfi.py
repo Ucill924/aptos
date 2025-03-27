29c9f7a7e2636671162bb9b331c5503176249652
Ucillny <72456107+Ucill924@users.noreply.github.com>
2025-03-28 00:21:47 +0700

    Create faucet-eigenfi.py

---
+++ faucet-eigenfi.py
@@ -0,0 +1,22 @@
+"sender": "0x4eff5e992f62b61789052fe2416ae351c82d82da769cb1cf461e53e1a6ef5674",
+    "sequence_number": "6",
+    "max_gas_amount": "2368",
+    "gas_unit_price": "100",
+    "expiration_timestamp_secs": "1743096005",
+    "payload": {
+        "function": "0xf7429cda18fc0dd78d0dc48b102158024f1dc3a511a2a65ea553b5970d65b028::eigenfi_token_minter::mint_fa",
+        "type_arguments": [],
+        "arguments": [
+            {
+                "inner": "0x9c9b084429eecf70c7c4f9b18980eb3cbb9c9a70fee7abfb59ca637005c5b430"
+            },
+            "10000000000"
+        ],
+        "type": "entry_function_payload"
+    },
+    "signature": {
+        "public_key": "0x425e6e4a05df6d75a8f4ddd6beaf8b1f6fcbd8032ff94358b2d234ff960ec773",
+        "signature": "0x0636eab7e354caddb8b522b98e5f0d1a7fa941716dac58aa7c4073a4fc6fbe04feeea2836a4852bfe666ac7c91b0802903288aabd4914064438b0fb7ece70f04",
+        "type": "ed25519_signature"
+    }
+}
